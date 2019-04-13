9fa35a6b6b15cdc938e8195224e9e81be26c6806
Alberto iglesias gallego <alberto.uchiha@gmail.com>
2019-04-13 20:28:02 +0200

    Deploy: Add secure encrypted password to travis.yml

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     packages=find_packages(exclude=['*tests']),
     include_package_data=True,
     install_requires=dependencies,
+    python_requires='>3.5.2',
     extras_require={
         'dev': dev_dependencies
     },
